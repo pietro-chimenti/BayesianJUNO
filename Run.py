813a17f2aa7d76ec673c144d2502523802223064
Pietro Chimenti <pietro.chimenti@gmail.com>
2020-08-11 14:44:20 -0300

    improving plots

--- Run.py
+++ Run.py
@@ -15,11 +15,13 @@ bja = BJ_loglike()
 
 # now plot the data 
 n_points = bja.BJ_LL_NOBS
-x_range = []
+x_range  = []
 y_vals   = []
+y_func   = []
 for x_ind in np.arange(0,n_points,1):
     x_range.append(bja.getXBin(int(x_ind)))
     y_vals.append(bja.getOb(int(x_ind)))
+    y_func.append(bja.f(bja.getXBin(int(x_ind))))
 plt.plot(x_range, y_vals)
 plt.show()
 
@@ -33,7 +35,10 @@ bja.generateRandom(19780126)
 for x_ind in np.arange(0,n_points,1):
     x_range[x_ind]= bja.getXBin(int(x_ind))
     y_vals[x_ind] = bja.getOb(int(x_ind))
-plt.plot(x_range, y_vals)
+    y_func[x_ind] = bja.f(bja.getXBin(int(x_ind)))
+plt. errorbar(x_range, y_vals, yerr = bja.BJ_LL_SIGMA, fmt='o', label='Data')  
+plt.plot(x_range,y_func, label='Theory')
+plt.legend()
 plt.show()
 
 
